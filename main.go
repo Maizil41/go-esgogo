4e836290125a6983cc8b7a3b01d810334ef3e1d7
Maizil41 <maizilpc7@gmail.com>
2024-11-12 10:56:16 +0700

    first commit

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func main() {
+	router := gin.Default()
+
+	router.Static("/static", "./static")
+
+	router.LoadHTMLGlob("templates/*")
+	router.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.html", nil)
+	})
+
+	router.Run(":8080")
+}
